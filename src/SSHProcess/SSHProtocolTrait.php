4934fbd7efd8c42aa25b8822f4e124fff5ab0c67
heimonsy <heimonsy@gmail.com>
2015-02-05 16:15:45 +0800

    catch exit status

--- src/SSHProcess/SSHProtocolTrait.php
+++ src/SSHProcess/SSHProtocolTrait.php
@@ -19,7 +19,7 @@ trait SSHProtocolTrait
         return  <<<EOD
 expect << EOF
 set timeout {$timeout}
-spawn {$command}
+eval exp_spawn {$command}
 while (1) {
     expect {
         timeout { puts stderr 'unknow error'; exit 1 }
@@ -33,6 +33,20 @@ while (1) {
     }
 }
 
+set ret [exp_wait]
+set pid         [lindex \\\$ret 0]
+set spawn_id    [lindex \\\$ret 1]
+set os_error    [lindex \\\$ret 2]
+set exit_status [lindex \\\$ret 3]
+
+puts stdout "\nExit Status: \\\$exit_status"
+puts stdout "OS Error: \\\$os_error"
+
+if {\\\$exit_status != 0} {
+    puts stderr "\nExit Status Not 0, Have Errors";
+    exit 1;
+}
+
 EOF
 EOD;
     }
@@ -44,7 +58,25 @@ EOD;
         return <<<EOD
 expect << EOF
 set timeout {$timeout}
-spawn {$command}
+
+proc judge {} {
+    set ret [exp_wait]
+    set pid         [lindex \\\$ret 0]
+    set spawn_id    [lindex \\\$ret 1]
+    set os_error    [lindex \\\$ret 2]
+    set exit_status [lindex \\\$ret 3]
+
+    puts stdout "\nExit Status: \\\$exit_status"
+    puts stdout "OS Error: \\\$os_error"
+
+    if {\\\$exit_status != 0} {
+        puts stderr "\nExit Status Not 0, Have Errors";
+        exit 1;
+    }
+    exit 0
+}
+
+eval exp_spawn {$command}
 expect {
     "ssh: connect" { puts stderr "ssh connect error"; exit 1 }
     timeout { puts stderr 'unknow error'; exit 1 }
@@ -53,6 +85,7 @@ expect {
     "password: " { puts stderr "ssh private key unrecognized"; exit 1 }
     "Enter passphrase" { send "{$passphrase}\r" }
     "(yes/no)? " { send "yes\r" }
+    eof { puts stderr "passphrase no use"; judge }
 }
 
 expect {
@@ -61,14 +94,14 @@ expect {
     "Permission denied" { puts stderr "private key error"; exit 1 }
     "password: " { puts stderr "ssh private key unrecognized"; exit 1 }
     "Enter passphrase" { send "{$passphrase}\r" }
-    eof { exit 0 }
+    eof { judge }
 }
 
 expect  {
     timeout { puts stderr 'unknow error'; exit 1 }
     "fatal: " { puts stderr "private key error"; exit 1 }
     "Permission denied" { puts stderr "private key error"; exit 1 }
-    eof { exit 0 }
+    eof { judge }
     "Enter passphrase" { puts stderr "private key passpharse not match"; exit 1 }
 }
 
